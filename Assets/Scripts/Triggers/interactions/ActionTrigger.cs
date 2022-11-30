d139ff54d2c3b0c4d3f2683be415907803732d5e
VenerioUvandy201401107 <veneriouvandy@gmail.com>
2022-11-30 18:33:20 +0700

    Update interaction button to all intractable object, use keyboard key "E" to interact.

--- Assets/Scripts/Triggers/interactions/ActionTrigger.cs
+++ Assets/Scripts/Triggers/interactions/ActionTrigger.cs
@@ -6,31 +6,22 @@ public class ActionTrigger : MonoBehaviour
 {
     [SerializeField] private SceneAction sceneAction = null;
 
-    private Collider2D hitbox;
-    private Vector2 hitpoint;
 
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<SpriteRenderer>().sprite = sceneAction.GetActionIcon();
 
-        hitbox = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            hitpoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Debug.Log("Click");
-
-            if (hitbox.OverlapPoint(hitpoint))
-            {
                 sceneAction.Interact();
 
                 this.gameObject.SetActive(false);
-            }
         }
     }
 }

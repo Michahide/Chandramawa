0e7ab8519997553f6718711b9704b64c8a7ee693
VenerioUvandy201401107 <veneriouvandy@gmail.com>
2022-12-01 23:37:31 +0700

    Death cutscene, delay text

--- Assets/Scripts/ShowDay.cs
+++ Assets/Scripts/ShowDay.cs
@@ -5,21 +5,24 @@ using UnityEngine.UI;
 
 public class ShowDay : MonoBehaviour
 {
-    [SerializeField] private float begin;
+    [SerializeField] private float begin = 0;
+    [SerializeField] private float close = 0;
+    [SerializeField] GameObject panel;
 
     void Start()
     {
         StartCoroutine(Show(begin));
+        StartCoroutine(Hide(close));
     }
-    IEnumerator Show(float delay)
+    IEnumerator Show(float show)
     {
-        yield return new WaitForSeconds(delay);
-        this.gameObject.SetActive(false);
+        yield return new WaitForSeconds(show);
+        this.panel.SetActive(true);
     }
     
-    IEnumerator Hide(float delay)
+    IEnumerator Hide(float hide)
     {
-        yield return new WaitForSeconds(delay);
-        this.gameObject.SetActive(false);
+        yield return new WaitForSeconds(hide);
+        this.panel.SetActive(false);
     }
 }
